ed8fe7261f70c685ad23da7b06adc836c8a56b50
Mathapelo Mokgomong <mmmokgomong@gmail.com>
2023-05-03 12:10:42 +0200

    Layout

---
+++ eCommerceWebsite/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceWebsite.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(SignInManager<IdentityUser> signInManager)
+        {
+            this._signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            
+            
+                return RedirectToAction("index", "home");
+            
+           
+
+           
+        }
+    }
+}
